ffcaa770f1ac0ab4654398f9d642eab6026a6730
icy-strong <jlatryce12@gmail.com>
2022-09-06 12:41:45 -0400

    first commit, refactoring soon

---
+++ reddit.py
@@ -0,0 +1,99 @@
+import praw
+
+import pyttsx3 as ptts
+from video import *
+import re
+
+from passw import *
+
+
+#default_subs = ["AmITheAsshole",'EntitledParents',"TIFU",'ProRevenge']
+default_subs = ["ProRevenge"]
+    
+#class that extracts the reddit posts using the praw api
+class Reddit:
+    def __init__(self,subs=default_subs, numPosts = 5):
+        self.reddit = praw.Reddit(
+            client_id= client_id,
+            client_secret =client_secret,
+            password=password,
+            user_agent=user_agent,
+            username=username
+        )
+        self.subs = subs
+        self.numPosts = numPosts
+        self.topPosts = None
+
+    def getSubs(self):
+        return self.subs
+
+    def getTopPosts(self, inclUpdates = False):
+        if not self.topPosts:
+            d = {}
+
+            #grabs top posts for the week from specified subs
+            for s in self.subs:
+                posts = list(self.reddit.subreddit(s).top("week", limit=self.numPosts))
+                if not inclUpdates:
+                    for post in posts:
+                        if "UPDATE" in post.title:
+                            posts.remove(post)
+                d[s] = posts
+            self.topPosts = d
+
+        return self.topPosts
+
+    #grabs top controversial posts for the week from specified subs    
+    def getControversialPosts(self):
+        d = {}
+        for s in self.subs:
+            d[s] = self.reddit.subreddit(s).controversial("week", limit=self.numPosts)
+        return d
+
+f=open("posts.txt",'w')
+
+red=Reddit(numPosts=2)     
+tp=red.getTopPosts()
+print('got top posts')
+
+#writes posts into text file
+for sub in tp:
+    print(sub)
+    for post in tp[sub]:
+        f.write(post.title+"\n")
+        f.write(post.selftext+'\n')
+
+
+f.close()        
+
+#replaces reddit new lines with normal new lines and removes any URL's, then rewrites the file
+f= open("posts.txt",'r+')
+testts= f.read().replace("&#x200B;", "\n")
+testts = re.sub(r'http\S+','',testts)
+f= open('posts.txt', 'w')
+f.write(testts)
+f.close()
+
+
+#intitalizes the voice robot and has it read the posts
+engine=ptts.init()
+voices=engine.getProperty('voices')
+rate=engine.getProperty('rate')
+#engine.setProperty('rate', rate-50)
+engine.save_to_file(testts, 'audio.mp3')
+engine.runAndWait()
+
+print('audio made')
+
+
+
+
+
+
+videoGen = vid(rate)
+
+videoGen.genV()
+videoGen.makeClips()
+videoGen.group()
+
+
---
+++ video.py
@@ -0,0 +1,93 @@
+from operator import sub
+from tkinter import font
+import moviepy.editor as mp
+
+
+#generates a video based off still image and audio, with subtitles
+class vid:
+    #make audio length in seconds
+    def __init__(self, wpm, imgs = ['stock.jpg']):
+        self.audioLength = None
+        self.v = None
+        self.imgs = imgs
+        self.wmp = wpm
+        self.clips=[]
+        self.finalVid = None
+    
+    #takes the audio and image, then combines them to make a video
+    def genV(self):
+    
+        audio_clip = mp.AudioFileClip('audio.mp3')
+        image_clip = mp.ImageClip('stock.jpg')
+        video_clip = image_clip.set_audio(audio_clip)
+        video_clip.duration = audio_clip.duration
+        video_clip.fps = 15
+        #video_clip.write_videofile('noSubtitles.mp4')
+        self.audioLength = audio_clip.duration
+        self.v=video_clip
+
+    #makes the clips in sections of how long the user specifies in LEN_CLIPS, and adds
+    #the amount of words based off the wpm count of video, and length of clips
+    def makeClips(self):
+        LEN_CLIPS=25
+        numClips = int(self.audioLength/LEN_CLIPS)
+        endLoop = numClips*LEN_CLIPS
+        excess = self.audioLength-(numClips*LEN_CLIPS)
+        wpc = int(self.wmp/2)
+        f = open('posts.txt','r')
+        script=[word for line in f for word in line.split()]
+        f.close()
+
+        print('numClips:', numClips)
+        print('len:',self.audioLength)
+
+        for i in range(0, numClips, 1):
+            cStart = i*LEN_CLIPS
+            clip = self.v.subclip(cStart,cStart+LEN_CLIPS+1)
+    
+            
+            
+            sStart = i*wpc
+            subScr = ''
+            numNewLines = 1
+            
+            for j in range(sStart, sStart+wpc, 1):
+                
+                subScr += script[j] + ' '
+                if len(subScr)//numNewLines >=50:
+                    subScr += '\n'
+                    numNewLines+=1
+
+            txt = mp.TextClip(subScr, color = 'white', fontsize = 25)
+            txt = txt.set_position('center')
+            finalC = mp.CompositeVideoClip([clip,txt])
+            finalC.duration=LEN_CLIPS
+            self.clips.append(finalC)
+
+
+        subScr=''
+        numNewLines=1
+        clip = self.v.subclip(endLoop, self.audioLength)
+        for i in range((numClips-1)*wpc, len(script),1):
+             subScr += script[i] + ' '
+             if len(subScr)//numNewLines >=50:
+                    subScr += '\n'
+                    numNewLines+=1
+
+        txt = mp.TextClip(subScr, color = 'white', fontsize=25 )
+        txt = txt.set_position('center')
+        finalC = mp.CompositeVideoClip([clip,txt])
+        finalC.duration = excess
+        self.clips.append(finalC)    
+
+
+    #takes all of the clips and combines them to make one full video
+    def group(self):
+        self.finalVid = mp.concatenate_videoclips(self.clips)
+        self.finalVid.write_videofile('vi.mp4')
+        
+
+
+
+
+
